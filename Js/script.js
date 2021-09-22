3a88b34398ce9200307c0d07c6ae2d5e1d2b4d82
FeDeRiCo9719 <federico.lovaglio.1997@gmail.com>
2021-09-22 15:34:07 +0200

    structure messages

--- Js/script.js
+++ Js/script.js
@@ -147,6 +147,48 @@ var app = new Vue({
                 ],
             },
             {
+                name: 'Marco',
+                avatar: '_3',
+                visible: true,
+                messages: [{
+                    date: '28/03/2020 10:10:40',
+                    message: 'La Marianna va in campagna',
+                    status: 'received'
+                },
+                    {
+                        date: '28/03/2020 10:20:10',
+                        message: 'Sicuro di non aver sbagliato chat?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '28/03/2020 16:15:22',
+                        message: 'Ah scusa!',
+                        status: 'received'
+                    }
+                ],
+            },
+            {
+                name: 'Luca',
+                avatar: '_1',
+                visible: true,
+                messages: [{
+                    date: '28/03/2020 10:10:40',
+                    message: 'La Marianna va in campagna',
+                    status: 'received'
+                },
+                    {
+                        date: '28/03/2020 10:20:10',
+                        message: 'Sicuro di non aver sbagliato chat?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '28/03/2020 16:15:22',
+                        message: 'Ah scusa!',
+                        status: 'received'
+                    }
+                ],
+            },
+            {
                 name: 'Luisa',
                 avatar: '_8',
                 visible: true,
